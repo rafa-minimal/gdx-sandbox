269aed4eb911a771223321ffae464c39c64ac0a1
Rafał Golcz <rafal.golcz@gmail.com>
2017-10-15 23:49:44 +0200

    GDX init (via gdx-setup.jar)

---
+++ android/src/com/minimal/sandbox/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.minimal.sandbox;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.minimal.sandbox.Sandbox;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Sandbox(), config);
+	}
+}
